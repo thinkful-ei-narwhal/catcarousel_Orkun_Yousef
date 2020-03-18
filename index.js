802f7d6e74e44e516b84a372b99ee75a3e48bb33
Orkun Ozekin <orkunozekin@thinkful.com>
2020-03-18 13:44:44 -0500

    Cat Carousel

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
-  });
+$('.thumbnail').on('click', e  => {
+    e.preventDefault();
   
+
+const imgSrc = $(e.currentTarget).find('img').attr('src');
+$('.hero img').attr('src', imgSrc);
+
+const imgAlt = $(e.currentTarget).find('alt').attr('alt');
+$('hero img').attr('img', imgAlt);
+
+});
